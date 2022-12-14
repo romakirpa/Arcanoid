36695b84aba6a83531dca7d4ff65a4e0fa6d623f
romakirpa <roman.kirpa.dev@gmail.com>
2022-12-14 13:31:28 +0200

    remove unused code in ball Scripts/Ball.cs

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -6,14 +6,12 @@ public class Ball : MonoBehaviour
     private Rigidbody _rigidBody;
     private float _speed;
     private Vector3 _direction;
-    private Transform _transform;
     private bool _isMoving;
     private float _currentDamage;
 
     private void Start()
     {
         _rigidBody = GetComponent<Rigidbody>();
-        _transform = GetComponent<Transform>();
         _speed = 20f;
         _isMoving = false;
         _direction = Vector3.up * _speed;
@@ -41,12 +39,6 @@ public class Ball : MonoBehaviour
     {
         var ricochet = Vector2.Reflect(_direction, normal);
         _direction = ricochet;
-    }
-
-    private void ToStartPosition()
-    {
-        _transform.position = new Vector3(0, 1.38f, 0);
-        _isMoving = false;
     }
 
     public void StartMove()
